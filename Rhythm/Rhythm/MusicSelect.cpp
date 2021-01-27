d78fde38387ccf70884083573ae879c2f89710e9
`sunakawa <koukin2130920yh@gmail.com>
2021-01-27 18:43:59 +0900

    Select Class add

--- Rhythm/Rhythm/MusicSelect.cpp
+++ Rhythm/Rhythm/MusicSelect.cpp
@@ -1,36 +1,92 @@
 #include "MusicSelect.h"
 
-
+//#define TEST
 
 
 MusicSelect::MusicSelect()
 {
 	input = new Input;
 
-	MusicName.push_back("うんち");
-	MusicName.push_back("うんち2");
-	MusicName.push_back("うんち3");
-
+	//背景&難易度用グラフィック
+	Back = LoadGraph("selectBack2.png");//背景
+	StarA = LoadGraph("selectStarA.png");//難易度(★)
+	StarB = LoadGraph("selectStarB.png");//難易度(☆)
+	//曲名-----
+	MusicName.push_back("曲1");
+	MusicName.push_back("曲2");
+	MusicName.push_back("曲3");
+	//難易度-----
 	Level.push_back("easy");
 	Level.push_back("normal");
 	Level.push_back("hard");
+	//曲のサムネイル(仮置き)
+	Music = LoadGraph("Music.png");
 }
 
+MusicSelect::~MusicSelect()
+{
+	delete input;
+}
+
+void MusicSelect::SelectDraw()
+{
+	DrawGraph(0, 0, Back, TRUE);
+
+	//難易度表示--------
+	DrawGraph(750, 850, easyflag, TRUE);
+	DrawGraph(900, 850, normalflag, TRUE);
+	DrawGraph(1050, 850, hardflag, TRUE);
+	if (LevelNumber == 0)easyflag = StarA;
+	else easyflag = StarB;
+	if (LevelNumber == 1)normalflag = StarA;
+	else normalflag = StarB;
+	if (LevelNumber == 2)hardflag = StarA;
+	else hardflag = StarB;
+
+	//スクロール----------
+	Right();
+	Center();
+	Left();
+	DrawExtendGraph(450 + ScrollX[1], 400 - ScrollY[1], 650 + (ScrollX[1] * 1.666667), 600, Music, TRUE);
+	DrawExtendGraph(750 + ScrollX[2], 200 - ScrollY[2], 1150 + ScrollX[3], 600, Music, TRUE);
+	DrawExtendGraph(1250 - ScrollX[0], 400 - ScrollY[0], 1450 - (ScrollX[0] * 0.6), 600, Music, TRUE);
+}
+
+#ifdef TEST
+
+void MusicSelect::leveldraw()
+{
+	DrawExtendGraph(450, 350, 650, 550, easyflag, TRUE);
+	DrawExtendGraph(850, 350, 1050, 550, normalflag, TRUE);
+	DrawExtendGraph(1250, 350, 1450, 550, hardflag, TRUE);
+
+	if (LevelNumber == 0)easyflag = StarA;
+	else easyflag = StarB;
+
+	if (LevelNumber == 1)normalflag = StarA;
+	else normalflag = StarB;
+
+	if (LevelNumber == 2)hardflag = StarA;
+	else hardflag = StarB;
+}
+
+#endif
+
+
+//曲情報を返す
 std::string MusicSelect::getnum()
 {
 	return MusicName[MusicNumber];
 }
-
 std::string MusicSelect::getlevel()
 {
 	return Level[LevelNumber];
 }
 
 
-
+//曲名とレベルの選択
 void MusicSelect::levelSelect()
 {
-
 	input->Initialize();
 
 	if (input->GetKeyDown(KEY_INPUT_RIGHT))
@@ -41,42 +97,179 @@ void MusicSelect::levelSelect()
 	{
 		LevelNumber--;
 	}
-
-	if (2 <= LevelNumber)
-	{
-		LevelNumber = 2;
-	}
-	if (0 >= LevelNumber)
-	{
-		LevelNumber = 0;
-	}
-
-
-	DrawFormatString(0, 40, GetColor(255, 255, 255), "%d", LevelNumber);
+	if (2 <= LevelNumber) LevelNumber = 2;
+	if (0 >= LevelNumber) LevelNumber = 0;
 }
-
 void MusicSelect::Select()
 {
-
 	input->Initialize();
 
 	if (input->GetKeyDown(KEY_INPUT_RIGHT))
 	{
-		MusicNumber++;
+
+		if (!Musicflag2) {
+			Musicflag = true;
+		}
 	}
 	else if (input->GetKeyDown(KEY_INPUT_LEFT))
 	{
-		MusicNumber--;
+
+		if (!Musicflag) {
+			Musicflag2 = true;
+		}
 	}
-
-	if (MusicName.size() <= MusicNumber)
-	{
-		MusicNumber = 0;
-	}
-
-
-	DrawFormatString(0, 0, GetColor(255, 255, 255), "%d", MusicNumber);
+	if (2 <= MusicNumber) MusicNumber = 2;
+	if (0 >= MusicNumber) MusicNumber = 0;
 }
+
+//スクロールの動き
+void MusicSelect::Left()//右から左へのスクロール0
+{
+	if (Musicflag) {
+		if (count[0] < 25)
+		{
+			if (ScrollX[0] < 500)
+			{
+				ScrollX[0] += 20;
+				ScrollY[0] += 8;
+			}
+			count[0]++;
+		}
+		else
+		{
+			MusicNumber++;
+			count[0] = 0;
+			Musicflag = false;
+		}
+	}
+	if (Musicflag2) {
+		if (count[0] < 25)
+		{
+			if (ScrollX[0] > -500)
+			{
+				ScrollX[0] -= 20;
+				ScrollY[0] -= 8;
+			}
+			count[0]++;
+		}
+		else
+		{
+			MusicNumber--;
+			count[0] = 0;
+			Musicflag2 = false;
+		}
+	}
+}
+void MusicSelect::Center()
+{
+	if (Musicflag)//右
+	{
+		if (!leftflag)
+		{
+			if (count[2] < 25)
+			{
+				if (ScrollX[0] < 500)
+				{
+					ScrollX[2] -= 12;
+					ScrollX[3] = ScrollX[2] * 1.6;
+					ScrollY[2] -= 8;
+				}
+				count[2]++;
+			}
+			else
+			{
+				count[2] = 0;
+				rightflag = true;
+			}
+		}
+		if (leftflag)
+		{
+			if (count[2] < 25)
+			{
+				ScrollX[2] -= 20;
+				ScrollX[3] = ScrollX[2] * 0.6;
+				ScrollY[2] += 8;
+				count[2]++;
+			}
+			else
+			{
+				count[2] = 0;
+				leftflag = false;
+			}
+		}
+	}
+	if (Musicflag2)
+	{
+		if (!rightflag)
+		{
+			if (count[2] < 25)
+			{
+				if (ScrollX[2] < 500)
+				{
+					ScrollX[2] += 20;
+					ScrollX[3] = ScrollX[2] * 0.6;
+					ScrollY[2] -= 8;
+				}
+				count[2]++;
+			}
+			else
+			{
+				leftflag = true;
+				count[2] = 0;
+			}
+		}
+		if (rightflag)
+		{
+			if (count[2] < 25)
+			{
+				ScrollX[2] += 12;
+				ScrollX[3] = ScrollX[2] * 1.6;
+				ScrollY[2] += 8;
+				count[2]++;
+			}
+			else
+			{
+				count[2] = 0;
+				rightflag = false;
+			}
+		}
+	}
+}
+void MusicSelect::Right()//左から右へのスクロール
+{
+	if (Musicflag2) {
+		if (count[1] < 25)
+		{
+			if (ScrollX[1] < 300)
+			{
+				ScrollX[1] += 12;
+				ScrollY[1] += 8;
+			}
+			count[1]++;
+		}
+		else
+		{
+			count[1] = 0;
+		}
+	}
+	if (Musicflag) {
+		if (count[1] < 25)
+		{
+			if (ScrollX[1] > -300)
+			{
+				ScrollX[1] -= 12;
+				ScrollY[1] -= 8;
+			}
+			count[1]++;
+		}
+		else
+		{
+			count[1] = 0;
+		}
+	}
+}
+
+
 
 
 
--- Rhythm/Rhythm/MusicSelect.h
+++ Rhythm/Rhythm/MusicSelect.h
@@ -7,17 +7,46 @@
 class MusicSelect
 {
 private:
-	int MusicNumber = 0;
+	int MusicNumber = 1;
 	int LevelNumber = 0;
+	//グラフィック
+	int Back;
+	int StarA;
+	int StarB;
+
+	int Music;
+	bool Musicflag = false;
+	bool Musicflag2 = false;
+	//難易度用フラグ
+	int easyflag;
+	int normalflag;
+	int hardflag;
+	//スクロール
+	int ScrollX[4] = { 0 };
+	int ScrollY[3] = { 0 };
+	int count[3] = { 0 };
+
+	bool leftflag = false;
+	bool rightflag = false;
+
 	Input *input;
+
 	std::vector<std::string> MusicName;
 	std::vector<std::string> Level;
 
 public:
 
-	void Select();
+	void Left();
+	void Center();
+	void Right();
+
+	~MusicSelect();
 	MusicSelect();
+
+	void Select();
 	void levelSelect();
+	void SelectDraw();
+	void leveldraw();
 	std::string getnum();
 	std::string getlevel();
 
