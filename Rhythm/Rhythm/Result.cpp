cf6ef97190f1a9f29de8d3f516695875b87810ff
mayu___cs <hato182@gmail.com>
2021-01-30 16:32:20 +0900

    Development result screen

--- Rhythm/Rhythm/Result.cpp
+++ Rhythm/Rhythm/Result.cpp
@@ -1,1 +1,61 @@
 #include "Result.h"
+#include "DxLib.h"
+#include <iostream>
+#include <string>
+
+Result::Result(const unsigned int MaxCombo, const unsigned int Judge[4], const unsigned int PlayScore, const unsigned int Notesize) : 
+	MaxCombo(MaxCombo),
+	PlayScore(PlayScore)
+{
+	for (auto i = 0; i < 4; i++) {
+		this->Judge[i] = Judge[i];
+	}
+	
+	//背景/フォント素材ロード
+	LoadDivGraph("Resources\\Font\\number.png", SCORE_SIZE, SCORE_SIZE, 1, 30, 44, Score_img);
+	LoadDivGraph("Resources\\Font\\rank.png", EVALUATION_SIZE, EVALUATION_SIZE, 1, 171, 226, Evaluation_img);
+
+	Background		= LoadGraph("Resources\\Background\\resultBack.png");
+	BackgroundMask	= LoadGraph("Resources\\Background\\resultBox.png");
+	Jacket			= LoadGraph("Resources\\MusicScore\\Data\\Lyrith -迷宮リリス-.png");
+
+	//得点割合計算
+	if (PlayScore != 0 && Notesize != 0) {
+		Base_MaxScore = Notesize * 220;
+		Percentage = (Base_MaxScore / PlayScore) * 100;
+	}
+	else {
+		Base_MaxScore = 0;
+		Percentage = 0;
+	}
+
+	if		(Percentage > 90) { Evaluation = EVALUATION_S; }
+	else if (Percentage > 80) { Evaluation = EVALUATION_A; }
+	else if (Percentage > 60) { Evaluation = EVALUATION_B; }
+	else					  { Evaluation = EVALUATION_C; }
+
+	Font = CreateFontToHandle("和田研細丸ゴシック2004絵文字P", 60, 0, DX_FONTTYPE_ANTIALIASING);
+	J_Font = CreateFontToHandle("和田研細丸ゴシック2004絵文字P", 40, 0, DX_FONTTYPE_ANTIALIASING);
+}
+
+void Result::Start()
+{
+	while (ScreenFlip() == false && ProcessMessage() == false && ClearDrawScreen() == false) {
+		//背景
+		DrawGraph(0, 0, Background, false);
+
+		//ジャケット
+		DrawExtendGraph(1150, 250, 1825, 925 , Jacket, true);
+
+		//リザルトボックス
+		DrawGraph(120, 250, BackgroundMask, true);
+
+		//スコア
+		DrawGraph(220, 280, Evaluation_img[Evaluation], true);
+		DrawStringToHandle(650, 835, std::to_string(PlayScore).c_str(), GetColor(255, 255, 255), Font);
+
+		for (auto i = 0; i < 4; i++) {
+			DrawStringToHandle(550, 560 + (i * 57), std::to_string(Judge[i]).c_str(), GetColor(255, 255, 255), J_Font);
+		}
+	}
+}
--- Rhythm/Rhythm/Result.h
+++ Rhythm/Rhythm/Result.h
@@ -1,5 +1,38 @@
 #pragma once
+
+#define SCORE_SIZE			10
+#define DISPLAY_SCORE_SIZE	6
+#define EVALUATION_SIZE		4
+
+#define PERFECT			0
+#define EXCELLENT		1
+#define GOOD			2
+#define BAD				3
+
+#define EVALUATION_S	3
+#define EVALUATION_A	2
+#define EVALUATION_B	1
+#define EVALUATION_C	0
+
 class Result
 {
-};
+private:
+	int Font;
+	int J_Font;
+	int Background;
+	int BackgroundMask;
+	int Jacket;
+	int Evaluation_img[EVALUATION_SIZE];
+	int Score_img[SCORE_SIZE];
 
+	unsigned int	PlayScore;
+	unsigned int	MaxCombo;
+	unsigned int	Judge[4];
+	unsigned int	Base_MaxScore;
+	unsigned int	Percentage;
+	unsigned short	Evaluation;
+
+public:
+	Result(const unsigned int MaxCombo, const unsigned int Judge[4], const unsigned int PlayScore, const unsigned int Notesize);
+	void Start();
+};
