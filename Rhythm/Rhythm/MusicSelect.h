ea6b67105309b0a3be08f75e362a532fad0582aa
`sunakawa <koukin2130920yh@gmail.com>
2021-01-30 21:13:30 +0900

    Revert "Revert "Revert "add Musicselect & Start"""
    This reverts commit bc761849c20c4a75f1adf5795ff936776a2e204b.

--- Rhythm/Rhythm/MusicSelect.h
+++ Rhythm/Rhythm/MusicSelect.h
@@ -8,7 +8,6 @@ class MusicSelect
 {
 private:
 	int MusicNumber = 1;
-	int oldMusicNumber = 0;
 	int LevelNumber = 0;
 	//グラフィック
 	int Back;
@@ -18,14 +17,8 @@ private:
 	int Music;
 	int Music1;
 
-	//スクロールさせる際のフラグ
-	bool Musicflag = false;//右
-	bool Musicflag2 = false;//左
-
-	//曲選択中かレベル選択中かの判定フラグ
-	bool determine = false;
-
-
+	bool Musicflag = false;
+	bool Musicflag2 = false;
 	//難易度用フラグ
 	int easyflag;
 	int normalflag;
@@ -37,26 +30,13 @@ private:
 
 	bool leftflag = false;
 	bool rightflag = false;
-	//フォント
-	int Font;
-	int LevelFont;
-
-	//サウンド
-	std::string Loadsound;
-	int sound;
-
 
 	Input *input;
 
 	std::vector<std::string> MusicName;
-	std::vector<std::string> MusicSound;
 	std::vector<std::string> Level;
 
-	bool aa = false;
-
 public:
-
-
 	//スクロール
 	void Left();
 	void Center();
@@ -65,15 +45,12 @@ public:
 	~MusicSelect();
 	MusicSelect();
 
-	void LoadMusic();
+	void LoadSelect();
 	void Select();
 	void levelSelect();
 	void SelectDraw();
 	void leveldraw();
 	void NameDraw();
-
-	//本部分
-	void LoadMusicSelect();
 
 	std::string getnum();
 	std::string getlevel();
--- Rhythm/Rhythm/Start.cpp
+++ Rhythm/Rhythm/Start.cpp
@@ -1,6 +1,5 @@
 #include "DxLib.h"
 
-#include "MusicSelect.h"
 #include "Start.h"
 #include "Result.h"
 #include "Play.h"
@@ -15,11 +14,6 @@ void Start::start()
 	Title title;
 	title.start();
 
-	MusicSelect select;
-	select.LoadMusicSelect();
-	std::string music = select.getnum();
-
-	Scene scene(music.c_str());
-	//Scene scene("Lyrith -–À‹{ƒŠƒŠƒX-");
+	Scene scene("Lyrith -–À‹{ƒŠƒŠƒX-");
 	scene.GameStart();
 }
